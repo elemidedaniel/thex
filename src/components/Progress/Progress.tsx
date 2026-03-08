41da6b4d5c0999710610e5f9813156e8e36d9724
Mac <mac@Daniel-mac.local>
2026-03-08 11:18:17 +0100

    Fix Progress import

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '../../tokens/cn';
